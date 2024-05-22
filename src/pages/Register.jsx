21ba457a3d2dd340eeb852d308179a540738a05a
Juantrevi <juantrevi70@gmail.com>
2024-05-22 17:24:24 +0200

    -- Register page finished --

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,30 @@
 import {Link} from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage.js";
+import Logo from "../components/Logo.jsx";
+import FormRow from "../components/FormRow.jsx";
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
-            <Link to={"/login"}>Login Page</Link>
-        </div>
+        <Wrapper>
+            <form className='form'>
+                <Logo />
+                <h4>Register</h4>
+
+                <FormRow type='text' name='name' labelText='Name' defaultValue='John' />
+                <FormRow type='text' name='lastName' labelText='Last Name' defaultValue='Doe' />
+                <FormRow type='text' name='location' labelText='Location' defaultValue='earth' />
+                <FormRow type='email' name='email' labelText='Email' defaultValue='john@email.com' />
+                <FormRow type='password' name='password' labelText='Password' defaultValue='secret123' />
+
+
+                <button type='submit' className='btn btn-block'>Submit</button>
+                    <p>
+                        Already a member?
+                        <Link to='/login' className='member-btn'>Login</Link>
+                    </p>
+
+            </form>
+        </Wrapper>
     )
 }
 
