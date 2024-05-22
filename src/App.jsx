fecdc52046c3713c60c6813471f21300cf687626
Juantrevi <juantrevi70@gmail.com>
2024-05-22 15:56:53 +0200

    -- Nested Routes --

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,30 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <HomeLayout/>,
-    },
-    {
-        path: "/landing",
-        element: <Landing/>,
-    },
-    {
-        path: "/register",
-        element: <Register/>,
-    },
-    {
-        path: "/login",
-        element: <Login/>,
-    },
-    {
-        path: "/dashboard",
-        element: <DashboardLayout/>,
+        children: [
+            {
+                index: true,
+                element: <Landing/>,
+            },
+            {
+                path: "register",
+                element: <Register/>,
+            },
+            {
+                path: "login",
+                element: <Login/>,
+            },
+            {
+                path: "dashboard",
+                element: <DashboardLayout/>,
+            },
+        ],
     },
     {
         path: "/error",
         element: <Error/>,
     },
-])
+]);
 
 const App = () => {
     return (
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,7 +1,10 @@
+import {Outlet} from "react-router-dom";
+
 const HomeLayout = () => {
     return (
         <div>
-        <h1>HomeLayout</h1>
+            <nav>Home Layout</nav>
+            <Outlet />
         </div>
     )
 }
