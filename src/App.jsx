ebc0c24ac3ed4496adcf8e89c5382b93cf6c93d7
Juantrevi <juantrevi70@gmail.com>
2024-05-22 13:27:05 +0200

    -- Router config --

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>home</h1>
+    },
+    {
+        path: "/about",
+        element:
+            <div>
+                <h1>about</h1>
+            </div>
+    },
+    {
+        path: "/contact",
+        element: <h1>contact</h1>
+    }
+])
+
 const App = () => {
     return (
 
-        <h1>Jobify App</h1>
+        <RouterProvider router={router}/>
     )
 }
 
