68be9043ee703f943a955e5d27ae0ca618d571c3
Juantrevi <juantrevi70@gmail.com>
2024-05-22 17:02:23 +0200

    -- Error page finished --

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,15 +1,33 @@
 import {Link, useRouteError} from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage.js";
+import img from "../assets/images/not-found.svg";
 
 const Error = () => {
     const error = useRouteError();
     console.log(error);
+
+    if (error.status === 404) {
+
+        return (
+            <Wrapper>
+                <div>
+                    <img src={img} alt="Not Found"/>
+                    <h3>Page not found</h3>
+                    <p>We can't seem to find the page you are looking for</p>
+                    <Link to="/dashboard">
+                        Back Home
+                    </Link>
+
+                </div>
+            </Wrapper>
+        )
+    }
     return (
-        <div>
-            <h1>Error</h1>
-            <p>{error.status}</p>
-            <p>{error.error.message}</p>
-            <Link to={"/"}>Go back to home</Link>
-        </div>
+        <Wrapper>
+            <div>
+                <h3>Something went wrong</h3>
+            </div>
+        </Wrapper>
     )
 }
 
