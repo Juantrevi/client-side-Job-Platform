812ea407558a5b2855b4450c7decbac468dade0a
Juantrevi <juantrevi70@gmail.com>
2024-05-23 18:31:23 +0200

    -- Dashboard structure navbar done --

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,10 +1,21 @@
 import {Outlet} from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard.js";
+import {BigSidebar, Navbar, SmallSidebar} from "../components/index.js";
 
 const DashboardLayout = () => {
     return (
-        <div>
-            <Outlet/>
-        </div>
+        <Wrapper>
+            <main className='dashboard'>
+                <SmallSidebar/>
+                <BigSidebar/>
+                <div>
+                    <Navbar/>
+                    <div className='dashboard-page'>
+                        <Outlet/>
+                    </div>
+                </div>
+            </main>
+        </Wrapper>
     )
 }
 
