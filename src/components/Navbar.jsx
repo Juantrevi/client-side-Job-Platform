092c9ad189d4b4d784ed0d7201cfabe07adc2761
Juantrevi <juantrevi70@gmail.com>
2024-05-24 20:42:08 +0200

    -- Logout and Dark/light theme toggle --

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import Wrapper from "../assets/wrappers/Navbar.js";
 import {FaAlignLeft} from "react-icons/fa";
 import Logo from "./Logo.jsx";
 import {useDashboardContext} from "../pages/DashboardLayout.jsx";
-
+import LogoutContainer from "./LogoutContainer.jsx";
+import ThemeToggle from "./ThemeToggle.jsx";
 const Navbar = () => {
     const {toggleSidebar} = useDashboardContext()
 
@@ -17,7 +18,8 @@ const Navbar = () => {
                     <h4 className='logo-text'>dashboard</h4>
                 </div>
                 <div className='btn-container'>
-                    toggle/logout
+                    <ThemeToggle />
+                    <LogoutContainer />
                 </div>
             </div>
         </Wrapper>
--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -11,7 +11,7 @@ const DashboardLayout = () => {
         name: 'John Doe'
     }
     const [showSidebar, setShowSidebar] = useState(false)
-    const [isDarkTheme, setisDarkTheme] = useState(false)
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
 
 
     const toggleDarkTheme = () => {
